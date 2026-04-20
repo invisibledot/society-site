97d23a1cec1fb1125a3b0b0f8b89e49f909300b6
invisibledot <stephen.edward.crowe@gmail.com>
2026-04-20 17:31:20 +0200

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: "static",
-
+  adapter: netlify(),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
